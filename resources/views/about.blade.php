674aba0382d1dfb516471311697e8bcecde13315
Osixty <alzidan057@gmail.com>
2022-01-24 08:18:33 +0700

    first commit

---
+++ resources/views/about.blade.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>HALAMAN ABOUT !!!!!</title>
+</head>
+<body>
+    <h1>HALAMAN ABOUT!</h1>  
+    <h3> <?=$nama;?> </h3>
+    <p>
+    <img src="images/<?= $gambar;?>" alt= "" width="200px">  
+
+
+</body>
+</html>
---
+++ resources/views/home.blade.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Home</title>
+
+    <!-- Costume CSS  -->
+    <link rel="stylesheet" href="css/style.css">
+
+
+</head>
+<body>
+    <h1>HOME</h1> 
+    <p>HALLO SAYA COCO!!</p>
+
+    <img src="images/talenta.jpg" alt="coco" width="500px">
+
+    <!-- Costume Scrip HERE -->
+    <script src="js/script.js"></script>
+    
+</body>
+</html>
