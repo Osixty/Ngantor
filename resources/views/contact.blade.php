9088fb619b64c816e76d212739383978096f967d
Osixty <alzidan057@gmail.com>
2022-02-15 21:00:47 +0700

    modul 4

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -6,21 +6,22 @@
             <div class="card border-0 shadow-lg">
                 <div class="card-body">
                     <h1>Contact Us</h1>
-                    <form action="Login.php" method="post" class="user login mt-5">
+                    <form action="{{route('contact.store')}}" method="post" class="user login mt-5">
+                        {{csrf_field()}}
                         <div class="form-group">
                             <label>Nama</label>
-                            <input type="text" id="input-nama" class="form-control form-control-user ">
+                            <input type="text" id="input-nama" class="form-control form-control-user " name="nama">
                         </div>
                         <div class="form-group">
                             <label>E-mail</label>
-                            <input type="email" id="input-email" class="form-control form-control-user">
+                            <input type="email" id="input-email" class="form-control form-control-user" name="email">
                         </div>
                         <div class="form-group">
-                            <textarea class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
+                            <textarea class="form-control" id="exampleFormControlTextarea1" rows="3" name="pesan"></textarea>
                             <label for="exampleFormControlTextarea1" class="form-label">Pesan</label>
                         </div>
                         <div class="text-right">
-                            <button type="submit" class="btn btn-primary">Submit</button>
+                            <button type="submit" class="btn btn-primary">Kirim</button>
                         </div>
                     </form>
                 </div>
